dc81a297fb91ead4ec324cea175fe90012924e21
Anton Gimpel <water34545@yandex.ru>
2022-05-14 23:25:44 +0400

    add worker with docker conf

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  prUser: process.env.PGUSER,
+  prHost: process.env.PGHOST,
+  prDatabase: process.env.PGDATABASE,
+  prPassword: process.env.PGPASSWORD,
+  prPort: process.env.PGPORT
+};
